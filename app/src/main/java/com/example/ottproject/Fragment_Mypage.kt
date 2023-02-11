85332a9788ec02525aab1cfd00eb784a07f5bd40
Hyemin Jeon <rlfl0225@nate.com>
2023-02-12 06:02:23 +0900

    First commit

--- app/src/main/java/com/example/ottproject/Fragment_Mypage.kt
+++ app/src/main/java/com/example/ottproject/Fragment_Mypage.kt
@@ -15,7 +15,7 @@ import com.example.ottproject.databinding.FragmentHomeBinding
 import com.example.ottproject.databinding.FragmentMypageBinding
 
 
-class Frgment_Mypage : Fragment() {
+class Fragment_Mypage : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
