32344a2cc91ba14c01c157006afe2db917445c0b
Hyemin Jeon <rlfl0225@nate.com>
2023-02-19 03:37:05 +0900

    Best commit

--- app/src/main/java/com/example/ottproject/Fragment_Home.kt
+++ app/src/main/java/com/example/ottproject/Fragment_Home.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
 
 class Fragment_Home : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 }
 
